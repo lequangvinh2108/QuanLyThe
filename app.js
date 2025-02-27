4d0c22efa7e3086c6b353d1b2d89ff96ca6ae9db
lequangvinh2108 <122371598+lequangvinh2108@users.noreply.github.com>
2025-02-27 10:21:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const cors = require ("cors");
 const databaseRouter = require("./app/router/database.router");
+const ApiError = require ("./app/api-error");
+
 
 const app = express ();
 
@@ -9,6 +11,20 @@ app.use (express.json());
 
 
 app.use("/api/database", databaseRouter);
+
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500.json({
+        message: error.message || "Internal Server Error",
+    }));
+});
+
+
+
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Quanlythe"});
