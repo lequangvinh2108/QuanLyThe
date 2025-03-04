01e54ebeecec8d6062fba434a6012f8f708c134e
lequangvinh2108 <122371598+lequangvinh2108@users.noreply.github.com>
2025-03-04 10:45:13 +0700

    Done

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const app = require ("./app");
 const config = require ("./app/config");
+const MongoDB = require ("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running port ${PORT}.`);
-});
+
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connect to database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to database!", error);
+        process.exit();
+    }
+}
+
+startServer();
+
+
